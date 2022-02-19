18c6eedeb26bb0a5dfb1b21731dcf87f814493df
Job <bojlk2@hotmail.com>
2022-02-18 21:52:21 -0600

    DB Connection

---
+++ index.js
@@ -0,0 +1,21 @@
+require ('dotenv').config()
+const mongoose = require('mongoose')
+const server = require('./src/server')
+
+const {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME
+} = process.env
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
+    .then(() => {
+        console.log('Connect to DB')
+    })
+    .catch(error => {
+        console.error('Error conection: ', error)
+    })
+server.listen(8080, () => {
+    console.log('Medium is ready on http://localhost:8080')
+})
